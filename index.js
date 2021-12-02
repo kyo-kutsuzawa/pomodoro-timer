435ed1506fdae1f600d6da749239292616a92f84
Kyo Kutsuzawa <kutsuzawa@tohoku.ac.jp>
2021-12-02 14:26:54 +0900

    Edit codes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const duration = 25 * 60 * 1000;
 
 window.onload = function() {
     startTime = Date.now();
-    setInterval("countDown()", 20);
+    countDown();
+    setInterval("countDown()", 50);
 }
 
 function countDown()
@@ -11,8 +12,16 @@ function countDown()
     const elapsedTime = Date.now() - startTime;
     const remainedTime = duration - elapsedTime;
 
-    const remainedTimeMin = Math.floor(remainedTime / 1000 / 60);
-    const remainedTimeSec = Math.floor(remainedTime / 1000) - remainedTimeMin * 60;
+    let remainedTimeMin = 0;
+    let remainedTimeSec = 0;
+    if (remainedTime > 0) {
+        remainedTimeMin = Math.floor(remainedTime / 1000 / 60);
+        remainedTimeSec = Math.floor(remainedTime / 1000) - remainedTimeMin * 60;
+    }
+    else {
+        remainedTimeMin = Math.floor(-remainedTime / 1000 / 60);
+        remainedTimeSec = Math.floor(-remainedTime / 1000) - remainedTimeMin * 60;
+    }
 
     let progress = remainedTime / duration * 100;
     if (progress < 0.0) {
@@ -23,6 +32,12 @@ function countDown()
     }
 
     const element = document.getElementById("timer");
-    element.style.backgroundImage = `radial-gradient(#dfe6e9 60%, transparent 60.5%), conic-gradient(#0984e3 0% ${progress}%, #dfe6e9 ${progress + 0.1}% 100%)`;
-    element.innerText = `${remainedTimeMin}:${remainedTimeSec}`;
+    element.style.backgroundImage = `radial-gradient(#dfe6e9 60%, #dfe6e900 60.5%), conic-gradient(#0984e3 0% ${progress}%, #dfe6e9 ${progress + 0.1}% 100%)`;
+
+    if (remainedTime > 0) {
+        element.innerText = `${remainedTimeMin}:${("0" + remainedTimeSec).slice(-2)}`;
+    }
+    else {
+        element.innerText = `-${remainedTimeMin}:${("0" + remainedTimeSec).slice(-2)}`;
+    }
 }
